86358c601a5b02bf31ed6ce4fd618456ebc45e56
Epharus Ndumia <epharusndumia@gmail.com>
2023-05-19 08:28:50 +0300

    initial commit

--- app.rb
+++ app.rb
@@ -1,3 +1,9 @@
+#print "Hello World!"
+#print "Hello World"
+#print "Hello world"
+
+
+
 # pp [{ id: 1, hello: "World" }, 
 #     { id: 2, hello: "Ruby" }, 
 #     { id: 3, hello: "Moon" }, 
@@ -14,10 +20,19 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+#def add num1, num2
+   # puts num1
+    #puts num2
+    #num1 + num2
+#end
 
-puts add(1,3) + add(2,2)
+#puts add(1,3) + add(2,2)
+
+
+#p [1,2,3]
+
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+#app.rb
+puts "Hello World!"
+p [1,2,3]
+print "Pass this test, please."
